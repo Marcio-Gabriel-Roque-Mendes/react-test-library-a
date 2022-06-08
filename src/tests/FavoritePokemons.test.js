dc7d450d70a7c5979c2cd694084cce4102c24d8f
Marcio Gabriel Roque Mendes <marcio_2411@hotmail.com>
2022-06-08 19:25:55 -0300

    requisito 2 realizado

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,5 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
 
+test('Teste se é exibida na tela a mensagem No favorite pokemon found',
+  () => {
+    // acessar os elementos da tela
+    renderWithRouter(<FavoritePokemons />);
+    const msgNaoEncontrado = screen.getByText('pokemon fou', { exact: false });
+
+    // fazer os testes
+    expect(msgNaoEncontrado).toBeInTheDocument();
+  });
+
+// it('')
 // acessar os elementos da tela
 
 // interagir com os elementos (se necessario)
