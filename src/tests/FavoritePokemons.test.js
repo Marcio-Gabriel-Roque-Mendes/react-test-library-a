4a7b0e9a68fa163a6dccf948117e6395a43b29ac
Marcio Gabriel Roque Mendes <marcio_2411@hotmail.com>
2022-06-08 20:45:17 -0300

    requisito 4 feito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import { screen } from '@testing-library/react';
-import renderWithRouter from '../renderWithRouter';
-import FavoritePokemons from '../pages/FavoritePokemons';
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// import renderWithRouter from '../renderWithRouter';
+// import FavoritePokemons from '../pages/FavoritePokemons';
 
-test('Teste se é exibida na tela a mensagem No favorite pokemon found',
-  () => {
-    // acessar os elementos da tela
-    renderWithRouter(<FavoritePokemons />);
-    const msgNaoEncontrado = screen.getByText('pokemon fou', { exact: false });
+// test('Teste se é exibida na tela a mensagem No favorite pokemon found',
+//   () => {
+//     // acessar os elementos da tela
+//     renderWithRouter(<FavoritePokemons />);
+//     const msgNaoEncontrado = screen.getByText('pokemon fou', { exact: false });
 
-    // fazer os testes
-    expect(msgNaoEncontrado).toBeInTheDocument();
-  });
+//     // fazer os testes
+//     expect(msgNaoEncontrado).toBeInTheDocument();
+//   });
 
-// it('')
-// acessar os elementos da tela
+// it('Teste se são exibidos todos os cards de pokémons favoritados',
+//   () => {
+//     // acessar os elementos da tela
+//     renderWithRouter(<FavoritePokemons />);
+//     // FALTA ESSE SEGUNDO TESTE
+//   });
 
-// interagir com os elementos (se necessario)
+// // acessar os elementos da tela
 
-// fazer os testes
+// // interagir com os elementos (se necessario)
+
+// // fazer os testes
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,7 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
 
-// acessar os elementos da tela
+test('Teste se a página contém um heading h2 com o texto Page requested not found',
+  () => {
+    // acessar os elementos da tela
+    renderWithRouter(<NotFound />);
+    const headingNotFound = screen.getByText(/Page requested not found/i);
+    // fazer os testes
+    expect(headingNotFound).toBeInTheDocument();
+  });
+
+it('Teste se a página mostra a imagem',
+  () => {
+    // acessar os elementos da tela
+    renderWithRouter(<NotFound />);
+    const getImage = screen
+      .getByAltText(/Pikachu crying because the page requested was not found/i);
+
+    // fazer os testes
+    expect(getImage).toBeInTheDocument();
+  });
 
 // interagir com os elementos (se necessario)
-
-// fazer os testes
