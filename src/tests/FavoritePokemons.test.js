254af42a56c0f2ea8107aeb741e39f1b35ccfe7c
Marcio Gabriel Roque Mendes <marcio_2411@hotmail.com>
2022-06-09 14:25:51 -0300

    requisito 3 solucionado

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,27 +1,28 @@
-// import React from 'react';
-// import { screen } from '@testing-library/react';
-// import renderWithRouter from '../renderWithRouter';
-// import FavoritePokemons from '../pages/FavoritePokemons';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import pokemons from '../data';
 
-// test('Teste se é exibida na tela a mensagem No favorite pokemon found',
-//   () => {
-//     // acessar os elementos da tela
-//     renderWithRouter(<FavoritePokemons />);
-//     const msgNaoEncontrado = screen.getByText('pokemon fou', { exact: false });
+test('Teste se é exibida na tela a mensagem No favorite pokemon found',
+  () => {
+    // acessar os elementos da tela
+    renderWithRouter(<FavoritePokemons />);
+    const msgNaoEncontrado = screen.getByText('pokemon fou', { exact: false });
 
-//     // fazer os testes
-//     expect(msgNaoEncontrado).toBeInTheDocument();
-//   });
+    // fazer os testes
+    expect(msgNaoEncontrado).toBeInTheDocument();
+  });
 
-// it('Teste se são exibidos todos os cards de pokémons favoritados',
-//   () => {
-//     // acessar os elementos da tela
-//     renderWithRouter(<FavoritePokemons />);
-//     // FALTA ESSE SEGUNDO TESTE
-//   });
+it('Teste se são exibidos todos os cards de pokémons favoritados',
+  () => {
+    // acessar os elementos da tela
+    renderWithRouter(<FavoritePokemons pokemons={ [pokemons[0]] } />);
+    const pikachu = screen.getByText(/pikachu/i);
+    // // interagir com os elementos (se necessario)
+    expect(pikachu).toBeInTheDocument();
+  });
 
 // // acessar os elementos da tela
-
-// // interagir com os elementos (se necessario)
 
 // // fazer os testes
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,20 +1,35 @@
-import React from 'react';
-import { screen } from '@testing-library/react';
-import renderWithRouter from '../renderWithRouter';
-import Pokedex from '../pages/Pokedex';
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// import renderWithRouter from '../renderWithRouter';
+// import Pokedex from '../pages/Pokedex';
+// import pokemons from '../data';
 
-test('Teste se a página contém um heading h2 com o texto Encountered pokémons',
-  async () => {
-    // acessar os elementos da tela
-    renderWithRouter(<Pokedex />);
-    const heading = await screen.getByRole('heading', { name: /Encountered pokémons/i });
+// test('Teste se a página contém um heading h2 com o texto Encountered pokémons',
+//   () => {
+//     // acessar os elementos da tela
+//     const favorites = {
+//       4: false,
+//       10: false,
+//       23: false,
+//       25: true,
+//       65: false,
+//       78: false,
+//       143: false,
+//       148: false,
+//       151: false,
+//     };
+//     renderWithRouter(<Pokedex
+//       isPokemonFavoriteById={ favorites }
+//       pokemons={ pokemons }
+//     />);
+//     const heading = screen.getByRole('heading', { name: /Encountered pokémons/i });
 
-    // fazer os testes
-    expect(heading).toBeInTheDocument();
-  });
+//     // fazer os testes
+//     expect(heading).toBeInTheDocument();
+//   });
 
-// acessar os elementos da tela
+// // acessar os elementos da tela
 
-// interagir com os elementos (se necessario)
+// // interagir com os elementos (se necessario)
 
-// fazer os testes
+// // fazer os testes
