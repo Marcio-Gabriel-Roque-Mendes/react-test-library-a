0938e706e44f265e1ee88059b1733f4a62ed6ec2
Marcio Gabriel Roque Mendes <marcio_2411@hotmail.com>
2022-06-21 14:45:49 -0300

    req 5 com correções feitas

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import renderWithRouter from '../renderWithRouter';
-import Pokemon from '../components/Pokemon';
-import pokemons from '../data';
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import renderWithRouter from '../renderWithRouter';
+// import Pokemon from '../components/Pokemon';
+// import pokemons from '../data';
 
-describe('Teste se é renderizado um card com as informações de determinado pokémon',
-  () => {
-    test('', () => {});
-  });
+// describe('Teste se é renderizado um card com as informações de determinado pokémon',
+//   () => {
+//     test('', () => {});
+//   });
 
-// acessar os elementos da tela
+// // acessar os elementos da tela
 
-// interagir com os elementos (se necessario)
+// // interagir com os elementos (se necessario)
 
-// fazer os testes
+// // fazer os testes
